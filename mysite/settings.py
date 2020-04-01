5b95f85025d18dd5a49a7ccefac70b30b41cdbf8
Nafiul Islam <gamesbrainiac@gmail.com>
2020-04-01 13:41:51 +0200

    Adding pools to list of apps.

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
