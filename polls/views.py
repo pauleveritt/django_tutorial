998eef95bcbb24aa00a168a60145892534df8263
Nafiul Islam <gamesbrainiac@gmail.com>
2020-04-01 13:42:12 +0200

    Adding type for question variable.

--- polls/views.py
+++ polls/views.py
@@ -36,7 +36,7 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question: Question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
